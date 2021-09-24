2c926df615cc68ca39b30630e2a514ededaf820e
Oleg Kireev <okireev@ubic.tech>
2021-09-24 11:12:20 +0300

    App component changes background color based on timer state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import './theme/theme.scss';
+import { useSelector } from 'react-redux';
+import { selectIsWorking } from './features/timer/store/selectors';
+import cx from 'classnames';
 import styles from './App.module.scss';
+import './theme/theme.scss';
 
 import Timer from './features/timer';
 
 function App() {
+	const isTimerActive = useSelector(selectIsWorking);
+
 	return (
-		<div className={styles.App}>
+		<div className={cx(styles.App, isTimerActive && styles.active)}>
 			<div className={styles.AppContainer}>
 				<Timer/>
 			</div>
