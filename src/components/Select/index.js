363f7d06cc49f02f5a601fe9ca7bf459a0813b69
Oleg Kireev <okireev@ubic.tech>
2021-09-25 18:11:52 +0300

    Create <Select />

---
+++ src/components/Select/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+import styles from './Select.module.scss';
+
+const Select = () => {
+	const seconds = new Array(60).fill('').map((_,i) => i);
+
+	return (
+		<Swiper
+			className={styles.Swiper}
+			direction='vertical'
+			slidesPerView={5}
+			spaceBetween={15}
+			loop
+			loopAdditionalSlides={60}
+			centeredSlides
+			freeMode
+			freeModeSticky
+			freeModeMomentumBounceRatio={5}
+			freeModeMomentumRatio={0.75}
+			// slideActiveClass={styles.SlideActive}
+			onSlideChange={(swiper) => console.log(swiper.realIndex)}
+			onSwiper={(swiper) => console.log(swiper)}
+		>
+			<div className={styles.target}></div>
+			{seconds.map((second) => <SwiperSlide className={styles.Slide} key={second}>{second < 10 ? `0${second}` : second}</SwiperSlide>)}
+		</Swiper>
+    
+	);
+};
+
+export default Select;
