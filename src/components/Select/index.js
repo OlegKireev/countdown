d051d1931ed83d19866c8fb853debb4a287a8067
Oleg Kireev <okireev@ubic.tech>
2021-09-26 20:20:26 +0300

    Add <Select /> onChange callback

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.min.css';
 import styles from './Select.module.scss';
+import PropTypes from 'prop-types';
 
-const Select = () => {
-	const seconds = new Array(60).fill('').map((_,i) => i);
+const Select = ({items, onChange}) => {
 
 	return (
 		<Swiper
@@ -20,14 +20,17 @@ const Select = () => {
 			freeModeMomentumBounceRatio={5}
 			freeModeMomentumRatio={0.75}
 			// slideActiveClass={styles.SlideActive}
-			onSlideChange={(swiper) => console.log(swiper.realIndex)}
-			// onSwiper={(swiper) => console.log(swiper)}
+			onSlideChangeTransitionEnd={(swiper) => onChange(swiper.realIndex)}
 		>
 			<div className={styles.target}></div>
-			{seconds.map((second) => <SwiperSlide className={styles.Slide} key={second}>{second < 10 ? `0${second}` : second}</SwiperSlide>)}
-		</Swiper>
-    
+			{items.map((second) => <SwiperSlide className={styles.Slide} key={second}>{second < 10 ? `0${second}` : second}</SwiperSlide>)}
+		</Swiper>  
 	);
+};
+
+Select.propTypes = {
+	items: PropTypes.array,
+	onChange: PropTypes.func,
 };
 
 export default Select;
