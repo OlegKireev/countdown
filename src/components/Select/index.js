144126a1efde802c4f5ac429214089cb6a33602e
Oleg Kireev <okireev@ubic.tech>
2021-09-26 20:48:22 +0300

    Improve <Select/> performance

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -18,9 +18,9 @@ const Select = ({items, onChange}) => {
 			freeMode
 			freeModeSticky
 			freeModeMomentumBounceRatio={5}
-			freeModeMomentumRatio={0.75}
 			// slideActiveClass={styles.SlideActive}
 			onSlideChangeTransitionEnd={(swiper) => onChange(swiper.realIndex)}
+			// onSlideChange={(swiper) => onChange(swiper.realIndex)}
 		>
 			<div className={styles.target}></div>
 			{items.map((second) => <SwiperSlide className={styles.Slide} key={second}>{second < 10 ? `0${second}` : second}</SwiperSlide>)}
