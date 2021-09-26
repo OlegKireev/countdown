f231e8b41a54911e6c98fc7b67e145a505683add
Oleg Kireev <okireev@ubic.tech>
2021-09-26 20:22:24 +0300

    Refactor <SetTimeControls />

--- src/features/timer/components/SetTimeControls/index.js
+++ src/features/timer/components/SetTimeControls/index.js
@@ -1,20 +1,24 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
+import Select from '../../../../components/Select';
 import { setMilliseconds } from '../../store/timerSlice';
 import styles from './SetTimeControls.module.scss';
 
 const SetTimeControls = () => {
-	const [secondsValue, setSecondsValue] = useState(0);
 	const dispatch = useDispatch();
 
-	const onSubmit = (e) => {
-		e.preventDefault();
-		dispatch(setMilliseconds(secondsValue  * 1000));
+	const onSelectChange = (value) => {
+		dispatch(setMilliseconds(value  * 1000));
 	};
+
+	const seconds = new Array(60).fill('').map((_,i) => i);
+
 	return (
-		<form className={styles.SetTimeControls} onSubmit={onSubmit}>
-			<input type='number' value={secondsValue} onChange={(e) => setSecondsValue(+e.target.value)}/>
-		</form>);
+		<div className={styles.SetTimeControls}>
+			<div style={{display: 'flex'}}>
+				<Select items={seconds} onChange={onSelectChange}/>				
+			</div>
+		</div>);
 };
 
 export default SetTimeControls;
