1a364eb6419b4bc711c9a9868bc89fe9ee7c44f7
Oleg Kireev <okireev@ubic.tech>
2021-09-27 10:35:41 +0300

    Add hidding of the <SetTimeControls /> while countdown is working

--- src/features/timer/components/SetTimeControls/index.js
+++ src/features/timer/components/SetTimeControls/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
 import Select from '../../../../components/Select';
 import { setMilliseconds } from '../../store/timerSlice';
 import styles from './SetTimeControls.module.scss';
 
-const SetTimeControls = () => {
+const SetTimeControls = ({isWorking}) => {
 	const dispatch = useDispatch();
 
 	const onSelectChange = (value) => {
@@ -14,11 +16,15 @@ const SetTimeControls = () => {
 	const seconds = new Array(60).fill('').map((_,i) => i);
 
 	return (
-		<div className={styles.SetTimeControls}>
+		<div className={cx(styles.SetTimeControls, isWorking && styles.isWorking)}>
 			<div style={{display: 'flex'}}>
 				<Select items={seconds} onChange={onSelectChange}/>				
 			</div>
 		</div>);
+};
+
+SetTimeControls.propTypes = {
+	isWorking: PropTypes.bool,
 };
 
 export default SetTimeControls;
